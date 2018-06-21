fa2b3a4b0741ab3f537694b21822719cc6ca70d9
IO89 <osipov.ivan.89@gmail.com>
2018-06-21 14:59:25 +0300

    Greeting new user and notify others

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ const socket = io();
 socket.on("connect", function() {
   console.log("connected to server");
 
-  socket.emit("createMessage", {
-    from: "Morris",
-    text: "Hey pretty!"
-  });
 });
 
 socket.on("disconnect", function() {
