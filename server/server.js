799c44c6fc01bbe350c0523079eff1af791181e8
IO89 <osipov.ivan.89@gmail.com>
2018-06-24 13:53:24 +0300

    add geolocation support

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
 
-const { generateMessage } = require("./utils/message");
+const { generateMessage, generateLocationMessage } = require("./utils/message");
 const publicPath = path.join(__dirname, "../public");
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -25,12 +25,14 @@ io.on("connection", socket => {
   socket.on("createMessage", (message, callback) => {
     console.log("createMessage", message);
     io.emit("newMessage", generateMessage(message.from, message.text));
-    callback('this is from server');
-    // socket.broadcast.emit("newMessage", {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
+    callback("this is from server");
+  });
+
+  socket.on("createLocationMessage", coords => {
+    io.emit(
+      "newLocationMessage",
+      generateLocationMessage("Admin", coords.latitude, coords.longitude)
+    );
   });
 
   socket.on("disconnect", () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require("expect");
 
-const { generateMessage } = require("./message");
+const { generateMessage, generateLocationMessage } = require("./message");
 
 describe("generateMessage", () => {
   it("should generate correct message object", () => {
@@ -10,5 +10,20 @@ describe("generateMessage", () => {
 
     expect(typeof message.createdAt).toBe("number");
     expect(message).toMatchObject({ from, text });
+  });
+});
+
+describe("generatelocationMessage", () => {
+  it("should generate correct location object", () => {
+    let from = "Morris";
+    let latitude = 12;
+    let longitude = 18;
+    let url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+    let location = generateLocationMessage(from, latitude, longitude);
+
+    expect(typeof location.createdAt).toBe("number");
+
+    expect(location.url).toBe(url);
+    expect(location).toMatchObject({ from, url });
   });
 });
